a1ff29944047d980e9abdf25b8ee41f09934a3ba
Joseph Kaguri <joseph@pezesha.com>
2022-08-28 23:56:09 +0300

    Update closevideo.js
    added tooltip function

--- closevideo.js
+++ closevideo.js
@@ -1,3 +1,10 @@
+$("#show-popup-button").on("click", function () {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+});
+
+// this stops youtube playing when closing modal
+
 $("body").on("hide.bs.modal", ".modal", function () {
   const $modal = $(this);
   // return early if there were no embedded YouTube videos
